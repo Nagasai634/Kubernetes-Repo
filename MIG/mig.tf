6cb1a60bf19f05bd47bd9dac58d954d2241deb1e
Nagasaivardhan <nagasaikonderu@gmail.com>
2026-01-08 15:03:11 +0530

    Refactor MIG Terraform configuration for clarity

--- MIG/mig.tf
+++ MIG/mig.tf
@@ -1,29 +1,29 @@
 provider "google" {
-    project = "intrepid-axe-482904-f4"
-    region = "us-central1"
-    credentials = file("harness-svc.json")
+  project     = "intrepid-axe-482904-f4"
+  region      = "us-central1"
+  credentials = file("harness-svc.json")
 }
 
 data "google_compute_network" "net" {
-    name = "default"
-  
+  name = "default"
+
 }
 resource "google_compute_firewall" "firewall" {
-    name = "allow-http"
-    network = data.google_compute_network.net.name
-    allow {
-      ports = ["80"]
-      protocol = "tcp"
-    }
-    source_ranges = ["0.0.0.0/0"]
+  name    = "allow-http"
+  network = data.google_compute_network.net.name
+  allow {
+    ports    = ["80"]
+    protocol = "tcp"
+  }
+  source_ranges = ["0.0.0.0/0"]
 
 }
 
 resource "google_compute_health_check" "health" {
-    name = "allow-health-check"
-    http_health_check {
-      port = 80
-    }
+  name = "allow-health-check"
+  http_health_check {
+    port = 80
+  }
 
 }
 
@@ -33,8 +33,8 @@ resource "google_compute_instance_template" "instance_temp" {
 
   disk {
     source_image = "projects/ubuntu-os-cloud/global/images/family/ubuntu-2204-lts"
-    boot        = true
-    auto_delete = true
+    boot         = true
+    auto_delete  = true
   }
 
   network_interface {
@@ -54,51 +54,51 @@ resource "google_compute_instance_template" "instance_temp" {
 
 
 resource "google_compute_instance_group_manager" "MIG" {
-    name = "apache2-mig"
-    zone = "us-central1-a"
-    base_instance_name = "apache"
-    version {
-      instance_template = google_compute_instance_template.instance_temp.id
-    }
-    target_size = 2
-    auto_healing_policies {
-      health_check = google_compute_health_check.health.id
-      initial_delay_sec = 120
-    }
-  
+  name               = "apache2-mig"
+  zone               = "us-central1-a"
+  base_instance_name = "apache"
+  version {
+    instance_template = google_compute_instance_template.instance_temp.id
+  }
+  target_size = 2
+  auto_healing_policies {
+    health_check      = google_compute_health_check.health.id
+    initial_delay_sec = 120
+  }
+
 }
 
 resource "google_compute_backend_service" "service" {
-    name = "backend-svc"
-    protocol = "HTTP"
-    port_name = "http"
-    health_checks = [google_compute_health_check.health.id]
-    backend {
-      group = google_compute_instance_group_manager.MIG.instance_group
-    }
-  
+  name          = "backend-svc"
+  protocol      = "HTTP"
+  port_name     = "http"
+  health_checks = [google_compute_health_check.health.id]
+  backend {
+    group = google_compute_instance_group_manager.MIG.instance_group
+  }
+
 }
 
 resource "google_compute_url_map" "url" {
-    name = "apache2"
-    default_service = google_compute_backend_service.service.id
+  name            = "apache2"
+  default_service = google_compute_backend_service.service.id
 
 }
 
 resource "google_compute_target_http_proxy" "http_proxy" {
-    name = "target-url"
-    url_map = google_compute_url_map.url.id
+  name    = "target-url"
+  url_map = google_compute_url_map.url.id
 }
 
 resource "google_compute_global_forwarding_rule" "ford_rule" {
-    name = "global-rule"
-    target = google_compute_target_http_proxy.http_proxy.id
-    port_range = "80"
-  
+  name       = "global-rule"
+  target     = google_compute_target_http_proxy.http_proxy.id
+  port_range = "80"
+
 }
 
 
 
 output "load_balancer_ipaddress" {
-    value = google_compute_global_forwarding_rule.ford_rule.ip_address
+  value = google_compute_global_forwarding_rule.ford_rule.ip_address
 }
